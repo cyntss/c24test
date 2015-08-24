85dcef779aadbbd18a1732c863d78d7bdf07a288
Cynthia Sanchez <cynt005@gmail.com>
2015-08-24 11:41:46 +0200

    New: starting the object

--- assets/js/task.js
+++ assets/js/task.js
@@ -1,3 +1,18 @@
 (function() {
-	console.log('Hello console');
+
+	var neighborhoodSize = 10000;
+	var gossipMaxSize = 20;
+	var groupsMaxAmount = 500;
+	
+
+	function CreateGossipGroups (totalPeople, maxPeoplePerGroup, maxNumberOfGroups) {
+		this.neighborhood = totalPeople;
+		this.people = maxPeoplePerGroup;
+		this.groups = maxNumberOfGroups;
+	}
+
+	var gossipGroups = new CreateGossipGroups(neighborhoodSize, gossipMaxSize, groupsMaxAmount);
+
+	console.log(gossipGroups.neighborhood, gossipGroups.people, gossipGroups.groups)
+
 })();
