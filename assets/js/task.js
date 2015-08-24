4b2ad4fe756b362735c460df08df283d3235fb5b
Cynthia Sanchez <cynt005@gmail.com>
2015-08-24 12:39:40 +0200

    New and fix: fixed values removed and added the constructor to create the groups of gossip groups in the neighborhood

--- assets/js/task.js
+++ assets/js/task.js
@@ -1,18 +1,35 @@
 (function() {
 
-	var neighborhoodSize = 10000;
-	var gossipMaxSize = 20;
-	var groupsMaxAmount = 500;
+	function CreateGossipGroups (maxPeople, maxPeoplePerGGroup, maxNumberOfGGroups) {
+		this.neighborhood = Math.floor((Math.random() * maxPeople) + 1);
+		
+		function createGroups (neighborhood) {
+			var minNumberOfGroups = neighborhood/maxPeoplePerGGroup;
+			var maxNumberOfGroups = neighborhood > maxNumberOfGGroups ? maxNumberOfGGroups : neighborhood;
+			this.gossipGroupsArray = [];
+			
+			console.log('Min number of gossip groups: ' + minNumberOfGroups);
+			console.log('Max number of gossip groups: ' + maxNumberOfGGroups);
+
+			var peopleInNeighborhood = neighborhood;
+			var i = 0;
+
+			while (peopleInNeighborhood > 0 && i < 2) {
+				peopleInThisGroup = Math.floor((Math.random() * maxPeoplePerGGroup) + 1)
+				console.log(peopleInThisGroup)
+				peopleInNeighborhood =  peopleInNeighborhood - peopleInThisGroup;
+				console.log(peopleInNeighborhood)
+				this.gossipGroupsArray.push(peopleInThisGroup)
+				i++;
+			}
+		}
+
+		var groupsCount = new createGroups(this.neighborhood);
+		this.groups = groupsCount.gossipGroupsArray
+	};
+
+	var gossipGroups = new CreateGossipGroups(10000, 20, 500);
+
+	console.log('Neighborhood population: ' + gossipGroups.neighborhood);
 	
-
-	function CreateGossipGroups (totalPeople, maxPeoplePerGroup, maxNumberOfGroups) {
-		this.neighborhood = totalPeople;
-		this.people = maxPeoplePerGroup;
-		this.groups = maxNumberOfGroups;
-	}
-
-	var gossipGroups = new CreateGossipGroups(neighborhoodSize, gossipMaxSize, groupsMaxAmount);
-
-	console.log(gossipGroups.neighborhood, gossipGroups.people, gossipGroups.groups)
-
 })();
