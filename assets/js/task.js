e9b31c91c207d49d72e6b09567d6a9be786e586e
Cynthia Sanchez <cynt005@gmail.com>
2015-08-24 13:24:34 +0200

    Removed: jquery, wont need it. New: arrays of gossip groups finished

--- assets/js/task.js
+++ assets/js/task.js
@@ -2,6 +2,7 @@
 
 	function CreateGossipGroups (maxPeople, maxPeoplePerGGroup, maxNumberOfGGroups) {
 		this.neighborhood = Math.floor((Math.random() * maxPeople) + 1);
+		console.log('Neighborhood population: ' + this.neighborhood);
 		
 		function createGroups (neighborhood) {
 			var minNumberOfGroups = neighborhood/maxPeoplePerGGroup;
@@ -14,22 +15,41 @@
 			var peopleInNeighborhood = neighborhood;
 			var i = 0;
 
-			while (peopleInNeighborhood > 0 && i < 2) {
-				peopleInThisGroup = Math.floor((Math.random() * maxPeoplePerGGroup) + 1)
-				console.log(peopleInThisGroup)
-				peopleInNeighborhood =  peopleInNeighborhood - peopleInThisGroup;
-				console.log(peopleInNeighborhood)
-				this.gossipGroupsArray.push(peopleInThisGroup)
+			//random insertion of people in groups
+			while (peopleInNeighborhood > 0 && i < maxNumberOfGroups) {
+				randomNumOfPeople = Math.floor((Math.random() * maxPeoplePerGGroup) + 2);
+				peopleInThisGroup = randomNumOfPeople > peopleInNeighborhood ? peopleInNeighborhood : randomNumOfPeople;
+				peopleInNeighborhood = peopleInNeighborhood - peopleInThisGroup;
+				this.gossipGroupsArray.push(peopleInThisGroup);
 				i++;
-			}
-		}
+			};
+			//are there people left in no groups? if yes, lets put them in groups
+			var e = 1;
+			while (peopleInNeighborhood > 0) {
+				console.log('People not listed in a gossip groups. Adding more. ' +e+ ' round: ' + peopleInNeighborhood);
+				this.gossipGroupsArray.forEach(function(element, index){
+					
+					if (element < maxPeoplePerGGroup && peopleInNeighborhood > 0) {
+						var maxNumberOfPeopleToAdd = maxPeoplePerGGroup - element;
+						var randomToAdd = Math.floor((Math.random() * maxNumberOfPeopleToAdd) + 1);
+						var peopleToAdd = randomToAdd > peopleInNeighborhood ? peopleInNeighborhood : randomToAdd;
+						element = peopleToAdd;
+						peopleInNeighborhood = peopleInNeighborhood - peopleToAdd;
+					}
+					
+				});
+				e++;
+		  };
+
+		};
 
 		var groupsCount = new createGroups(this.neighborhood);
-		this.groups = groupsCount.gossipGroupsArray
+		this.groupsArray = groupsCount.gossipGroupsArray;
+		this.quantityOfGroups = groupsCount.gossipGroupsArray.length;
 	};
 
 	var gossipGroups = new CreateGossipGroups(10000, 20, 500);
-
-	console.log('Neighborhood population: ' + gossipGroups.neighborhood);
+	console.log('Array of gossip groups: ' + gossipGroups.groupsArray);	
+	console.log('Number of gossip groups created: ' + gossipGroups.quantityOfGroups);
 	
 })();
